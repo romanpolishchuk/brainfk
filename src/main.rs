5d8775906a1435238ced4f312312fc5d895e8915
Roman Polishchuk <roman.polishchuk.work@gmail.com>
2025-11-07 05:06:09 +0000

    Optimize algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,150 @@
+use std::time::Instant;
+
+#[derive(PartialEq)]
+enum Opcode {
+    Right(u64),
+    Left(u64),
+    Plus(u64),
+    Minus(u64),
+    Out,
+    Input,
+    OpenLoop(usize, usize),
+    CloseLoop(usize, usize),
+    None,
+}
+
 fn main() {
+    let timer = Instant::now();
+
     let mut ram: Vec<u8> = vec![0; 1000000];
-    let mut stack: Vec<usize> = vec![];
     let mut data_pointer = 0 as usize;
     let mut program_pointer = 0 as usize;
 
     let program: Vec<char> = std::fs::read_to_string("./assets/program.bf")
         .unwrap()
         .chars()
+        .filter(|c| {
+            *c == '>'
+                || *c == '<'
+                || *c == '+'
+                || *c == '-'
+                || *c == '.'
+                || *c == ','
+                || *c == '['
+                || *c == ']'
+        })
         .collect();
 
+    let mut stack: Vec<usize> = vec![];
+    let mut opcodes: Vec<Opcode> = vec![];
     while program_pointer < program.len() {
         let opcode = program[program_pointer];
-        //println!("{}, {}", program_pointer, opcode);
         program_pointer += 1;
 
         match opcode {
             '>' => {
-                data_pointer += 1;
-            }
-            '<' => {
-                data_pointer -= 1;
-            }
-            '+' => {
-                ram[data_pointer] = ram[data_pointer].wrapping_add(1);
-            }
-            '-' => {
-                ram[data_pointer] = ram[data_pointer].wrapping_sub(1);
-            }
-            '.' => {
-                print!("{}", ram[data_pointer] as char);
-            }
-            ',' => {
-                unimplemented!(",");
-            }
-            '[' => {
-                if ram[data_pointer] == 0 {
-                    let mut count = 1;
-                    while count > 0 {
-                        let op = program[program_pointer];
-                        if op == '[' {
-                            count += 1;
-                        } else if op == ']' {
-                            count -= 1;
-                        }
-                        program_pointer += 1;
+                if let Some(op) = opcodes.last_mut() {
+                    if let Opcode::Right(n) = op {
+                        *n += 1;
+                    } else {
+                        opcodes.push(Opcode::Right(1));
                     }
                 } else {
-                    //println!("[ {}", program_pointer - 1);
-                    stack.push(program_pointer - 1);
+                    opcodes.push(Opcode::Right(1));
                 }
             }
-            ']' => {
-                //println!("] {}", program_pointer - 1);
-                if ram[data_pointer] != 0 {
-                    program_pointer = stack.pop().unwrap();
+            '<' => {
+                if let Some(op) = opcodes.last_mut() {
+                    if let Opcode::Left(n) = op {
+                        *n += 1;
+                    } else {
+                        opcodes.push(Opcode::Left(1));
+                    }
                 } else {
-                    stack.pop().unwrap();
+                    opcodes.push(Opcode::Left(1));
+                }
+            }
+            '+' => {
+                if let Some(op) = opcodes.last_mut() {
+                    if let Opcode::Plus(n) = op {
+                        *n += 1;
+                    } else {
+                        opcodes.push(Opcode::Plus(1));
+                    }
+                } else {
+                    opcodes.push(Opcode::Plus(1));
+                }
+            }
+            '-' => {
+                if let Some(op) = opcodes.last_mut() {
+                    if let Opcode::Minus(n) = op {
+                        *n += 1;
+                    } else {
+                        opcodes.push(Opcode::Minus(1));
+                    }
+                } else {
+                    opcodes.push(Opcode::Minus(1));
+                }
+            }
+            '.' => {
+                opcodes.push(Opcode::Out);
+            }
+            ',' => {
+                opcodes.push(Opcode::Input);
+            }
+            '[' => {
+                opcodes.push(Opcode::OpenLoop(opcodes.len(), 0));
+                stack.push(opcodes.len() - 1);
+            }
+            ']' => {
+                let open_index = stack.pop().unwrap();
+                if let Opcode::OpenLoop(open, _) = opcodes[open_index] {
+                    opcodes.push(Opcode::CloseLoop(opcodes.len(), open));
+                    opcodes[open] = Opcode::OpenLoop(open, opcodes.len() - 1);
                 }
             }
             _ => {}
         }
     }
+
+    program_pointer = 0;
+
+    while program_pointer < opcodes.len() {
+        let opcode = &opcodes[program_pointer];
+        program_pointer += 1;
+
+        match opcode {
+            Opcode::Right(count) => {
+                data_pointer += *count as usize;
+            }
+            Opcode::Left(count) => {
+                data_pointer -= *count as usize;
+            }
+            Opcode::Plus(count) => {
+                ram[data_pointer] = ram[data_pointer].wrapping_add(*count as u8);
+            }
+            Opcode::Minus(count) => {
+                ram[data_pointer] = ram[data_pointer].wrapping_sub(*count as u8);
+            }
+            Opcode::Out => {
+                print!("{}", ram[data_pointer] as char);
+            }
+            Opcode::Input => {
+                unimplemented!(",");
+            }
+            Opcode::OpenLoop(_, close_index) => {
+                if ram[data_pointer] == 0 {
+                    program_pointer = close_index + 1;
+                }
+            }
+            Opcode::CloseLoop(_, open_index) => {
+                if ram[data_pointer] != 0 {
+                    program_pointer = open_index + 1;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    println!("Took: {}s", timer.elapsed().as_secs_f32());
 }
